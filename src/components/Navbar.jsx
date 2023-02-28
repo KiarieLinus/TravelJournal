977449bd8353fda3509bcf12629b1a60edd50025
KiarieLinus <kiarielinus@gmail.com>
2023-02-28 18:43:34 +0300

    Navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import logo from "../assets/globe.svg"
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img src={logo} className='nav--logo' />
+            
+            <p>my travel journal.</p>
+        </nav>
+    )
+}
